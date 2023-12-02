bc21e7307ceaa4887fba4f4ead48afbedaa5bf74
Evan <eroberts1998@icloud.com>
2023-12-02 13:37:48 -0500

    step 3 complete: load network traffic from csv

--- CLI/src/main.py
+++ CLI/src/main.py
@@ -18,3 +18,16 @@ def load_network_from_csv(file_path):
         network.setdefault(end_node, []).append({'end_node': start_node, 'capacity': capacity, 'weight': weight})
 
     return network
+
+def load_network_traffic_from_csv(file_path):
+    df = pd.read_csv(file_path)
+
+    traffic = {}
+    for _, row in df.iterrows():
+        source_node = row['Source']
+        destination_node = row['Destination']
+        demand = row['Demand']
+
+        traffic.setdefault(source_node, {})[destination_node] = demand
+        
+    return traffic
---
+++ CLI/tests/test_load_traffic.py
@@ -0,0 +1,18 @@
+import pytest
+from src.main import load_network_traffic_from_csv
+
+def test_load_traffic_from_csv():
+    file_path = "CLI/data/example_traffic.csv"
+    traffic = load_network_traffic_from_csv(file_path)
+
+    # Test the presence of sources and destinations
+    assert 'A' in traffic
+    assert 'B' in traffic
+    assert 'I' in traffic
+    assert 'H' in traffic
+
+    # Test some traffic demands
+    assert traffic['A']['E'] == 1
+    assert traffic['B']['A'] == 3
+    assert traffic['I']['E'] == 2
+    assert traffic['H']['D'] == 4
