e3a63cbc8f3d6c9cbb36504ba1882ef01a72770b
Evan <eroberts1998@icloud.com>
2023-12-05 15:24:17 -0500

    update visualization to use tkinter over solely matplotlib and networkx

--- CLI/src/main.py
+++ CLI/src/main.py
@@ -1,7 +1,7 @@
 from resources.load import load_network_from_csv, load_network_traffic_from_csv
 from resources.report import model_traffic_flow, generate_report
 
-def main(network, traffic):
+def main():
     pass
 
 if __name__ == "__main__":
